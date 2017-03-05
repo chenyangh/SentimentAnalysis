ed69383b37a95b6d8c9889ea74a74306914f6eac
chenyangh <hchenyang@hotmail.com>
2017-03-05 13:57:27 -0700

    read-data

---
+++ data_init.py
@@ -0,0 +1,13 @@
+import json
+from datetime import datetime
+
+print(datetime.now())
+data_file_name = 'Electronics_5.json'  # 13 secs loading time
+# data_file_name = 'Digital_Music_5.json'  # 1 sec loading time
+
+f = open(data_file_name, 'r')
+for line in f.readlines():
+    json.loads(line)
+
+f.close()
+print(datetime.now())
