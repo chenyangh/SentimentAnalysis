f945224edcd85d1c5f100fd3dd4c176cfaf7e278
chenyangh <hchenyang@hotmail.com>
2017-03-08 03:04:18 -0700

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sentiment Analysis
+
+### Dataset
+1. [Amazon review dataset](http://jmcauley.ucsd.edu/data/amazon/)
+
+### Baseline
+1. Bag of Words
+
+
+### Acknowledgements
+* The **DeepLearningMovies-master** folder contains the code for [Kaggle](https://www.kaggle.com/c/word2vec-nlp-tutorial/details/part-1-for-beginners-bag-of-words)'s tutorial
--- data_init.py
+++ data_init.py
@@ -2,8 +2,8 @@ import json
 from datetime import datetime
 
 print(datetime.now())
-data_file_name = 'Electronics_5.json'  # 13 secs loading time
-# data_file_name = 'Digital_Music_5.json'  # 1 sec loading time
+# data_file_name = 'Electronics_5.json'  # 13 secs loading time
+data_file_name = 'Digital_Music_5.json'  # 1 sec loading time
 
 f = open(data_file_name, 'r')
 for line in f.readlines():
